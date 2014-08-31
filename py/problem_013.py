1661fe46d400e406484b813e5aacaa8af4cf6cba
Curtis Belmonte <curtislb@princeton.edu>
2014-08-31 16:58:43 -0400

    Solves Problem 13 in Python

---
+++ py/problem_013.py
@@ -0,0 +1,24 @@
+"""problem_013.py
+
+Problem 13: Large sum
+
+Work out the first D digits of the sum of tof the numbers contained in the file
+INPUT_FILE (all of which have the same number of digits).
+
+@author: Curtis Belmonte
+"""
+
+# import common
+
+# PARAMETERS ##################################################################
+
+INPUT_FILE = 'input/013.txt' # default: 'input/013.txt'
+
+# SOLUTION ####################################################################
+
+if __name__ == '__main__':
+    total = 0
+    with open(INPUT_FILE) as file:
+        for line in file:
+            total += int(line[:-1])
+    print(str(total)[:10])
