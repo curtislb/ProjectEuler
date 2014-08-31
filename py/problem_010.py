70832944dedd59c87a481f3dcb21f45ad347aafa
Curtis Belmonte <curtislb@princeton.edu>
2014-08-31 14:35:42 -0400

    Solves Problem 10 in Python

--- py/problem_010.py
+++ py/problem_010.py
@@ -1,17 +1,21 @@
-'''
-Problem 10.
+"""problem_010.py
+
+Problem 10: Summation of primes
 
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below LIMIT.
 
 @author: Curtis Belmonte
-'''
+"""
+
+import common
+
+# PARAMETERS ##################################################################
 
 LIMIT = 2000000 # default: 2000000
 
-###############################################################################
+# SOLUTION ####################################################################
 
-from common import primes_up_to
-
-print(sum(primes_up_to(LIMIT - 1)))
+if __name__ == '__main__':
+    print(sum(common.primes_up_to(LIMIT - 1)))
