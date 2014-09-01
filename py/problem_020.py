12f4bea99d5e9c3e1071a66f0421cf2f3a60a513
Curtis Belmonte <curtislb@princeton.edu>
2014-08-31 21:50:13 -0400

    Solves Problem 20 in Python

--- py/problem_020.py
+++ py/problem_020.py
@@ -1,20 +1,24 @@
-'''
-Problem 20.
+"""problem_020.py
 
-n! means n  (n  1)  ...  3  2  1
+Problem 20: Factorial digit sum
 
-For example, 10! = 10  9  ...  3  2  1 = 3628800,
-and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800, and the sum of the
+digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number N!
 
 @author: Curtis Belmonte
-'''
+"""
+
+import common
+
+# PARAMETERS ##################################################################
 
 N = 100 # default: 100
 
-###############################################################################
+# SOLUTION ####################################################################
 
-from common import factorial, sum_digits
-
-print(sum_digits(factorial(N)))
+if __name__ == '__main__':
+    print(common.sum_digits(common.factorial(N)))
