c4e8c7b09f2a319015efa8ae04f5c1bcf1ec6206
Curtis Belmonte <curtislb@princeton.edu>
2014-09-05 13:30:39 -0400

    Solves Problem 33 in Python

--- py/problem_033.py
+++ py/problem_033.py
@@ -17,8 +17,6 @@ value of the denominator.
 @author: Curtis Belmonte
 """
 
-import fractions
-
 import common
 
 # PARAMETERS ##################################################################
@@ -28,4 +26,51 @@ D = 2 # default: 2
 # SOLUTION ####################################################################
 
 if __name__ == '__main__':
-    pass
+    # compute minimum and maximum D-digit numbers
+    MIN_VALUE = 10**(D - 1)
+    MAX_VALUE = 10**D - 1
+    
+    # search for all fractions that satisfy the problem conditions
+    product_numer = 1
+    product_denom = 1
+    for numerator in range(MIN_VALUE, MAX_VALUE):
+        for denominator in range(numerator + 1, MAX_VALUE + 1):
+            # compute the properly reduced numerator and denominator
+            gcd = common.gcd(numerator, denominator)
+            reduced_numer = numerator // gcd
+            reduced_denom = denominator // gcd
+            
+            # cancel all cancelable digits of numerator and denominator
+            numer_str = str(numerator)
+            denom_str = str(denominator)
+            for i in range(D):
+                # does digit canceled fraction equal reduced fraction?
+                match_found = False
+                
+                for j in range(D):
+                    # check if digits can be canceled non-trivially
+                    if (numer_str[i] == denom_str[j] and numer_str[i] != '0'):
+                        # cancel matching digits in numerator and denominator
+                        canceled_numer = int(numer_str[:i] + numer_str[i + 1:])
+                        canceled_denom = int(denom_str[:j] + denom_str[j + 1:])
+                        
+                        # compute reduced value of digit canceled fraction
+                        gcd = common.gcd(canceled_numer, canceled_denom)
+                        reduced_canceled_numer = canceled_numer // gcd
+                        reduced_canceled_denom = canceled_denom // gcd
+                        
+                        # compare digit canceled fraction to reduced fraction
+                        if (reduced_canceled_numer == reduced_numer
+                            and reduced_canceled_denom == reduced_denom):
+                            # fractions match; add fraction to product
+                            product_numer *= reduced_numer
+                            product_denom *= reduced_denom
+                            match_found = True
+                            break
+                
+                # stop search if digit canceled match has already been found
+                if match_found:
+                    break
+    
+    # print the reduced denominator of product
+    print(product_denom // common.gcd(product_numer, product_denom))
