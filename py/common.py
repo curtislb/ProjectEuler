b7a1fb4bc1679b8534741b2662d6426b0e52043d
Curtis Belmonte <curtislb@princeton.edu>
2014-09-06 11:32:33 -0400

    Solves Problem 38 in Python

--- py/common.py
+++ py/common.py
@@ -544,7 +544,7 @@ def numbers_from_file(input_file):
         return matrix
 
 
-def pandigital_string(first, last):
+def pandigital_string(first = 1, last = 9):
     """Returns a string with each of the digits from first to last in order."""
     return ''.join('%d' % digit for digit in range(first, last + 1))
 
--- py/problem_038.py
+++ py/problem_038.py
@@ -1,11 +1,12 @@
-'''
-Problem 38.
+"""problem_038.py
+
+Problem 38: Pandigital multiples
 
 Take the number 192 and multiply it by each of 1, 2, and 3:
 
-192 * 1 = 192
-192 * 2 = 384
-192 * 3 = 576
+    192 × 1 = 192
+    192 × 2 = 384
+    192 × 3 = 576
 
 By concatenating each product we get the 1 to 9 pandigital, 192384576. We will
 call 192384576 the concatenated product of 192 and (1,2,3)
@@ -15,26 +16,50 @@ The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and
 (1,2,3,4,5).
 
 What is the largest 1 to 9 pandigital 9-digit number that can be formed as the
-concatenated product of an integer with (1,2, ... , n) where n > 1?
+concatenated product of an integer with (1, 2, ... , n) where n > 1?
 
 @author: Curtis Belmonte
-'''
-from common import INF, is_permutation, PAN_STR, str_permutations
+"""
 
-max_num = -INF
-for digits in range(1, 5):
-    for perm in str_permutations(PAN_STR, digits):
-        base_num = int(perm)
-        
-        n = 2
-        prod_str = str(base_num) + str(base_num * 2)
-        while len(prod_str) < 9:
-            n += 1
-            prod_str += str(base_num * n)
-        if len(prod_str) > 9:
-            continue
-        if is_permutation(prod_str, PAN_STR):
-            num = int(prod_str)
-            if num > max_num:
-                max_num = num
-print(max_num)
+import itertools
+
+import common
+
+# PARAMETERS ##################################################################
+
+# N/A
+
+# SOLUTION ####################################################################
+
+if __name__ == '__main__':
+    max_num = 0
+    PANDIGIT_STRING = common.pandigital_string()
+    
+    # try all starting numbers with up to 4 digits
+    for num_digits in range(1, 5):
+        # try all permutations of count_digits digits from pandigital string
+        for permutation in itertools.permutations(PANDIGIT_STRING, num_digits):
+            # form the starting number by joining the string digits
+            start_num = int(''.join(permutation))
+            
+            # search for n that gives the correct number of digits
+            n = 2
+            product_str = str(start_num) + str(start_num * 2)
+            while len(product_str) < 9:
+                # concatenate each new product to product_str
+                n += 1
+                product_str += str(start_num * n)
+            
+            # check if number of digits is greater than pandigital string
+            if len(product_str) > 9:
+                continue
+            
+            # check if concatenated products are 1 to 9 pandigital
+            if common.is_permutation(product_str, PANDIGIT_STRING):
+                num = int(product_str)
+                
+                # set new maximum pandigital number if necessart
+                if num > max_num:
+                    max_num = num
+     
+    print(max_num)
