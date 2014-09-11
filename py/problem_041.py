0fa9c1e63edad40df32f08b26af231f36faf81aa
Curtis Belmonte <curtislb@princeton.edu>
2014-09-10 20:11:31 -0400

    Solves Problem 41 in Python

--- py/problem_041.py
+++ py/problem_041.py
@@ -1,5 +1,6 @@
-'''
-Problem 41.
+"""problem_041.py
+
+Problem 41: Pandigital prime
 
 We shall say that an n-digit number is pandigital if it makes use of all the
 digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is
@@ -8,23 +9,34 @@ also prime.
 What is the largest n-digit pandigital prime that exists?
 
 @author: Curtis Belmonte
-'''
+"""
 
-# [problem-defined constants]
+import common
 
-###############################################################################
+# PARAMETERS ##################################################################
 
-from common import digits, primes_up_to, run_thread
+# N/A
 
-digit_sets = [{d for d in range(1, n)} for n in range(2, 11)]
+# SOLUTION ####################################################################
 
-n = -1
-primes = list(primes_up_to(7654321))
-p_digits = digits(primes[n])
-num_digits = len(p_digits)
-while not num_digits == sum(digit in p_digits
-                            for digit in digit_sets[num_digits - 1]):
-    n -= 1
-    p_digits = digits(primes[n])
-    num_digits = len(p_digits)
-print(primes[n])
+# Array of 1 to n pandigital numbers for n from 2 to 7
+PANDIGIT_STRINGS = [common.pandigital_string(1, n) for n in range(2, 8)]
+
+if __name__ == '__main__':
+    # compute primes up to maximum possible pandigital number
+    # note: 1 to 8 or 9 pandigital numbers cannot be prime (divisible by 3)
+    primes = common.primes_up_to(7654321)
+    
+    # check if each prime is 1 to n pandigital in decreasing order
+    i = -1
+    prime = primes[i]
+    digit_count = common.count_digits(prime)
+    while not common.is_permutation(str(prime),
+                                    PANDIGIT_STRINGS[digit_count - 2]):
+        # advance to next largest prime number
+        i -= 1
+        prime = primes[i]
+        digit_count = common.count_digits(prime)
+    
+    # print largest prime that satisfies the conditions
+    print(primes[i])
