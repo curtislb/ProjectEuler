28f6b3e211ee1728c14e06b68ccba174a0162910
Curtis Belmonte <curtislb@princeton.edu>
2014-10-30 01:45:04 -0400

    Solves Problem 47 (slowly) in Python

--- py/problem_047.py
+++ py/problem_047.py
@@ -1,40 +1,50 @@
-'''
-Problem 47.
+"""problem_047.py
+
+Problem 47: Distinct primes factors
 
 The first two consecutive numbers to have two distinct prime factors are:
 
-14 = 2 * 7
-15 = 3 * 5
+14 = 2 × 7
+15 = 3 × 5
 
 The first three consecutive numbers to have three distinct prime factors are:
 
-644 = 2**2 * 7 * 23
-645 = 3 * 5 * 43
-646 = 2 * 17 * 19.
+644 = 2^2 × 7 × 23
+645 = 3 × 5 × 43
+646 = 2 × 17 × 19.
 
-Find the first N consecutive integers to have N distinct primes factors. What
-is the first of these numbers?
+Find the first CONSEC consecutive integers to have FACTORS distinct prime
+factors. What is the first of these numbers?
 
 @author: Curtis Belmonte
-'''
+"""
 
-N = 4 # default: 4
+import collections
 
-###############################################################################
+import common
 
-from collections import deque
+# PARAMETERS ##################################################################
 
-from common import prime_factorization
+CONSEC = 4 # default: 4
+FACTORS = 4 # default: 4
 
-if N < 4:
-    n = (2, 14, 644)[N - 1]
-else:
-    n = 647
+# SOLUTION ####################################################################
 
-has_N_pfs = deque(len(prime_factorization(k).keys()) == N for k in range(n, n + N))
+if __name__ == '__main__':
+    n = 2
+    
+    # create buffer of whether property holds for first CONSEC numbers from n
+    buffer = collections.deque()
+    for i in range(n, n + CONSEC):
+        prime_factors = common.prime_factorization(n)
+        buffer.append(len(prime_factors) == FACTORS)
+    
+    # append new element and pop previous, until property holds for all CONSEC
+    while sum(buffer) != CONSEC:
+        n += 1
+        prime_factors = common.prime_factorization(n + (CONSEC - 1))
+        buffer.popleft()
+        buffer.append(len(prime_factors) == FACTORS)
+    
+    print(n)
 
-while sum(has_N_pfs) != N:
-    n += 1
-    has_N_pfs.popleft()
-    has_N_pfs.append(len(prime_factorization(n + (N - 1)).keys()) == N)
-print(n)
