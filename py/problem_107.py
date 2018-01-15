1d6424bdce065da8d7094db766657c8a6f071198
Curtis Belmonte <curtis.belmonte@gmail.com>
2018-01-14 17:48:51 -0800

    Fix #107 description formatting in Python

--- py/problem_107.py
+++ py/problem_107.py
@@ -7,7 +7,7 @@ Problem 107: Minimal network
 The following undirected network consists of seven vertices and twelve edges
 with a total weight of 243.
 
-    	A	B	C	D	E	F	G
+        A	B	C	D	E	F	G
     A	-	16	12	21	-	-	-
     B	16	-	-	17	20	-	-
     C	12	-	-	28	-	31	-
